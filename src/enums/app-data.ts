d7b8061ec3c14abf77445bc417de79088a536a51
projectcode.dev <noreply@projectcode.dev>
2026-06-03 16:34:47 +0530

    Update from projectcode.dev

--- src/enums/app-data.ts
+++ src/enums/app-data.ts
@@ -600,8 +600,8 @@ const MastersDegree: EducationSection = {
 
 export const AppConfig = {
     loaderSplashAnimation: true,        // enable or disable splash screen at the initialization of website
-    logoName: "Ragnar Lothbrok",         // Signature font logo name in header
-    name: "Ragnar Lothbrok",             // your name
+    logoName: "Bjorn Ironside",          // Signature font logo name in header
+    name: "Bjorn Ironside",              // your name
     emailId: "thedhruvilrathod@gmail.com",  // your email id
 
     // Google Form Contact Link
