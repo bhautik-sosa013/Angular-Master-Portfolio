46f31da503ca3c2c48104541a03521e02dc68ce6
projectcode.dev <noreply@projectcode.dev>
2026-06-03 14:30:08 +0530

    Update from projectcode.dev

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    APP_URL: "https://angular-master-portfolio.web.app",
-    API_URL: "https://common-server-9df8.onrender.com/",
-    RE_CAPTCHA_V3: "6LedUxcqAAAAAKsctlowzDQtHbN43D1y7s8eAyL8",
-};
